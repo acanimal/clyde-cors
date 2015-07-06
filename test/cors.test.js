c3c2d16ba85d7cab24c6bc658c3021ce19f5e5a3
acanimal <asantiago@acuriousanimal.com>
2015-07-06 22:52:58 +0200

    CORS filter implementation. Requires add configuration options.

---
+++ test/cors.test.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var path = require("path"),
+    request = require("supertest"),
+    http = require("http"),
+    clyde = require("clyde");
+
+
+describe("cors", function() {
+
+  var server;
+
+  afterEach(function() {
+    server.close();
+  });
+
+
+  it("should success applying cors headers", function(done) {
+    var options = {
+      port: 8888,
+      logfile: path.join(__dirname, "..", "tmp", "clyde.log"),
+      loglevel: "info",
+
+      prefilters: [
+        {
+          id: "cors",
+          path: path.join(__dirname, "../lib/index.js"),
+          config: {}
+        }
+      ],
+
+      providers: [
+        {
+          id: "id",
+          context: "/provider",
+          target: "http://server"
+        }
+      ]
+    };
+
+    // Create server with clyde's middleware options
+    var middleware = clyde.createMiddleware(options);
+    server = http.createServer(middleware);
+    server.listen(options.port);
+
+
+
+
+    // Make request which expects a provider not found error
+    request("http://localhost:8888")
+      .get("/foo")
+      .expect(404)
+      .expect("Access-Control-Allow-Origin", "*", done);
+
+  });
+
+
+});
